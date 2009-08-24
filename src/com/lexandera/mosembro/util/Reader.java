1ee114b5d2443af127da4ac7a575be6e632d7eb4
Aleksander Kmetec <aleksander.kmetec@gmail.com>
2009-08-24 20:06:55 +0000

    Rewrote some string reading functions using Scanner
    --HG--
    extra : convert_revision : svn%3A0521376e-d4f8-11dd-a3c7-fda619b15ae3/trunk%4045

--- src/com/lexandera/mosembro/util/Reader.java
+++ src/com/lexandera/mosembro/util/Reader.java
@@ -1,12 +1,11 @@
 package com.lexandera.mosembro.util;
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
 import android.content.res.Resources;
 
@@ -22,26 +21,11 @@ public class Reader
      */
     public static String readRawString(Resources res, int resourceId)
     {
-        InputStream is = res.openRawResource(resourceId);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is), 8192);
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-        
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                is.close();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
+    	StringBuilder sb = new StringBuilder();
+        Scanner s = new Scanner(res.openRawResource(resourceId));
+
+        while (s.hasNextLine()) {
+        	sb.append(s.nextLine() + "\n");
         }
         
         return sb.toString();
@@ -84,34 +68,22 @@ public class Reader
      */
     public static String readRemoteString(String fileUrl)
     {
-        InputStream is = null;
         StringBuilder sb = new StringBuilder();
-        String line = null;
         
         try {
             URLConnection connection = (new URL(fileUrl)).openConnection();
             connection.setConnectTimeout(10000);
             connection.setReadTimeout(10000);
             connection.connect();
-            is = connection.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is), 8192);
             
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
+            Scanner s = new Scanner(connection.getInputStream());
+            
+            while (s.hasNextLine()) {
+                sb.append(s.nextLine() + "\n");
             }
         }
         catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            try {
-                if (is != null) {
-                    is.close();
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
         }
         
         return sb.toString();
